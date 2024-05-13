7ef306c2b18c14a5cf152ee8b1b1361cf289ee75
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2024-05-13 16:29:33 +0100

    Added MLP via scikit-learn

--- test_evaluate_model_transfer.py
+++ test_evaluate_model_transfer.py
@@ -3,15 +3,22 @@ import numpy as np
 from evaluate_model_transfer import evaluate_model_transfer
 
 class TestEvaluateModelTransfer(unittest.TestCase):
-    def test_evaluate_model_transfer_accuracy(self):
-        data_folder = r'.'
-        parts = 2
-        null_window_center = np.nan
+    def setUp(self) -> None:
+        self.data_folder = r'.'
+        self.parts = 2
+        self.null_window_center = np.nan
+
+    def test_evaluate_model_transfer_accuracy_svm(self):
         classifier = 'multiclass-svm'
-        
-        accuracy = evaluate_model_transfer(data_folder, parts, null_window_center=null_window_center, classifier=classifier)
+        accuracy = evaluate_model_transfer(self.data_folder, self.parts, null_window_center=self.null_window_center, classifier=classifier)
         
         self.assertGreaterEqual(accuracy, 0.25, "Accuracy should be at least 0.25")
+
+    def test_evaluate_model_transfer_accuracy_scikit_mlp(self):
+        classifier = 'scikit-mlp'
+        accuracy = evaluate_model_transfer(self.data_folder, self.parts, null_window_center=self.null_window_center, classifier=classifier, components_pca=200)
+        
+        self.assertGreaterEqual(accuracy, 0.09, "Accuracy should be at least 0.09")
 
 if __name__ == '__main__':
     unittest.main()
